75c7a8b089bd90a07837733b6721d89b90029c27
Dharani Subramaniyan <dharanisubramaniyan25@gmail.com>
2025-03-03 10:44:16 +0530

    feature branch added

---
+++ Function.py
@@ -0,0 +1,51 @@
+
+# Function......
+
+''' Function is a block of code only runs when the function calls.
+    Can pass parameter in the function.
+    Function is defined using "def" keywords. '''
+
+# Create the function
+
+def fun(): 
+    pass
+
+# Call the function
+
+def fun():
+    print("Function")
+fun()                 # function calling
+
+
+#Arguments.......
+''' Can pass the arguments in the function inside the parenthesis. '''
+def fun(fruit, color):
+    print( f"{fruit}, {color}")
+fun( "cherry", "red")
+fun("Muskmelon", "yellow")
+
+# Arbitrary Arguments.....
+
+'''  arguments that allow a function to accept an unlimited number of arguments. 
+     add "*" symbol before the parameters.
+     for non keyword arguments. '''
+
+def multi(*num):
+    mul = 2
+    for x in num:
+          mul *= x
+    return mul
+print(multi(5, 3, 5))
+print(multi(2, 6))
+
+# arbitrary keyword argument.....
+
+''' arbitrary keyword arguments value pass to the function but identified by the key value'''
+
+def fun(**name):
+    print(name['name1'])
+fun(name1 = "Sandhiya")
+
+
+
+
